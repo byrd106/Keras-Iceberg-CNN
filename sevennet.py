9a22236d596441ba2b94c3f7efc30b8a76a317ee
byrd106 <byrd106@mail.chapman.edu>
2017-12-10 16:09:12 -0700

    seves

--- sevennet.py
+++ sevennet.py
@@ -18,7 +18,7 @@ from tensorflow.python.client import device_lib
 
 
 train_df = pd.read_json("data/processed/train.json")
-#test_df = pd.read_json("data/processed/test.json")
+test_df = pd.read_json("data/processed/test.json")
 
 x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_1"]])
 
@@ -90,14 +90,14 @@ plt.savefig(str(e)+'_sevennet.png')
 
 # next round of testing: 
 # Test data
-# x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_1"]])
-# x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_2"]])
-# X_test = np.concatenate([x_band1[:, :, :, np.newaxis], x_band2[:, :, :, np.newaxis]], axis=-1)
-# print("Xtest:", X_test.shape)
+x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_1"]])
+x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_2"]])
+X_test = np.concatenate([x_band1[:, :, :, np.newaxis], x_band2[:, :, :, np.newaxis]], axis=-1)
+print("Xtest:", X_test.shape)
 
-# prediction = model.predict(X_test, verbose=1)
-# submit_df = pd.DataFrame({'id': test_df["id"], 'is_iceberg': prediction.flatten()})
-# submit_df.to_csv("./second_submission.csv", index=False)
+prediction = model.predict(X_test, verbose=1)
+submit_df = pd.DataFrame({'id': test_df["id"], 'is_iceberg': prediction.flatten()})
+submit_df.to_csv("./sevennet_submission.csv", index=False)
 
 
 
