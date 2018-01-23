8bdfdb55e289246ab5b0b189992f9acdb63c32bc
byrd106 <byrd106@mail.chapman.edu>
2018-01-22 23:58:32 -0700

    create finalnet

--- finalNet.py
+++ finalNet.py
@@ -5,6 +5,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Convolution2D, GlobalAveragePooling2D, Dense, Dropout, MaxPooling2D, Flatten
 from keras.optimizers import Adam, SGD
+from sklearn.preprocessing import normalize
 
 import matplotlib
 matplotlib.use('Agg')
@@ -21,11 +22,25 @@ from tensorflow.python.client import device_lib
 train_df = pd.read_json("data/processed/train.json")
 #test_df = pd.read_json("data/processed/test.json")
 
-x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_1"]])
-x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_1"]])
-x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_2"]])
+
+normalize = True
+if normalize: 
+	print "NORMALIZE"
+	x_band1 = np.array([normalize(np.array(band).astype(np.float32).reshape(75, 75),norm='l2') for band in train_df["band_1"]])
+	x_band2 = np.array([normalize(np.array(band).astype(np.float32).reshape(75, 75),norm='l2') for band in train_df["band_2"]])
+else:
+	print "DONT NORMALIZE"
+	x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_1"]])
+	x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train_df["band_2"]])
+
+ 
+
 X_train = np.concatenate([x_band1[:, :, :, np.newaxis], x_band2[:, :, :, np.newaxis]], axis=-1)
+
+print X_train[0][:1]
+
 y_train = np.array(train_df["is_iceberg"])
+
 
 
 #print X_train.shape[0]
@@ -101,7 +116,7 @@ plt.savefig(str(e)+'_LOSS_'+sys.argv[1]+'.png')
 # next round of testing: 
 # Test data
 
-
+'''
 x_band1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_1"]])
 x_band2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test_df["band_2"]])
 X_test = np.concatenate([x_band1[:, :, :, np.newaxis], x_band2[:, :, :, np.newaxis]], axis=-1)
@@ -110,6 +125,6 @@ print("Xtest:", X_test.shape)
 prediction = model.predict(X_test, verbose=1)
 submit_df = pd.DataFrame({'id': test_df["id"], 'is_iceberg': prediction.flatten()})
 submit_df.to_csv("./finalNET_submission.csv", index=False)
-
+'''
 
 
