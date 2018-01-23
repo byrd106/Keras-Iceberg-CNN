10a131d80dc175b90d179b74ee17f20abf2f71f7
byrd106 <byrd106@mail.chapman.edu>
2018-01-22 21:17:41 -0700

    FNET

--- finalNet.py
+++ finalNet.py
@@ -59,22 +59,17 @@ model.add(Convolution2D(64, kernel_size=(3, 3), activation="relu", input_shape=(
 model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
 model.add(Dropout(0.2))
 
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
+model.add(Convolution2D(128, kernel_size=(3, 3), activation='relu' ))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.2))
 
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(Convolution2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.2))
 
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(Convolution2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.2))
-
-model.add(Convolution2D(64, 3, activation="relu"))
-model.add(Convolution2D(64, 3, activation="relu"))
-model.add(Convolution2D(64, 3, activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 
 
 model.add(GlobalAveragePooling2D())
