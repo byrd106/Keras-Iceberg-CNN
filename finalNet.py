ec6fb9dd7ce2b661bbf67109ed5ca2c016325f87
byrd106 <byrd106@mail.chapman.edu>
2018-01-22 22:03:07 -0700

    Update for final net

--- finalNet.py
+++ finalNet.py
@@ -34,41 +34,24 @@ y_train = np.array(train_df["is_iceberg"])
 
 model=Sequential()
 
-# Conv block 1
-model.add(Conv2D(64, kernel_size=(3, 3),activation='relu', input_shape=(75, 75, 2)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu' ))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu' ))
-model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-# Conv block 2
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Convolution2D(32, 3, activation="relu", input_shape=(75, 75, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 model.add(Dropout(0.2))
 
-# Conv block 3
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Convolution2D(64, 3, activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 model.add(Dropout(0.2))
 
-#Conv block 4
-model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Convolution2D(128, 3, activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
 model.add(Dropout(0.2))
 
-# Flatten before dense
-model.add(Flatten())
+model.add(GlobalAveragePooling2D())
 
-#Dense 1
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.4))
-
-#Dense 2
-model.add(Dense(512, activation='relu'))
+model.add(Dense(200, activation="relu"))
 model.add(Dropout(0.2))
-
-# Output 
+model.add(Dense(200, activation="relu"))
+model.add(Dropout(0.2))
 model.add(Dense(1, activation="sigmoid"))
 optimizer = Adam(decay=0.01)
 model.compile(optimizer, "binary_crossentropy", metrics=["accuracy"])
